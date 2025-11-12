8132fb63a7f833129c7ea0e907a86d63a9ef29b2
Jeffrey Sarnoff <jeffrey.sarnoff@gmail.com>
2025-11-12 08:06:45 -0500

    refactor

--- src/values.jl
+++ src/values.jl
@@ -11,21 +11,16 @@
 function valueseq(x::AIFloat{T, :unsigned, Δ}) where {T, Δ}
     # span width: how many fractionals we repeat per exponent
     stride = x.n_values ÷ 2^x.exp_bits
+    # poz is positive or zero
+    poz = non_negative(x::AIFloat, stride)
 
-    exponent_values   = exponents(x, stride)
-    fractional_values = fractionals(x)
-    implicit_bits     = implicits(x)
-
-    significand_values = fractional_values .+ implicit_bits
-    values = exponent_values .* significand_values
-
-    values[end] = T(NaN)
+    poz[end] = T(NaN)
     if Δ === :extended
         # next-to-last is +Inf
-        values[end-1] = T(Inf)
+        poz[end-1] = T(Inf)
     end
 
-    return values
+    return poz
 end
 
 #
@@ -34,26 +29,30 @@ end
 function valueseq(x::AIFloat{T, :signed, Δ}) where {T, Δ}
     # build the unsigned-like ladder first
     stride = x.n_values ÷ 2^x.exp_bits
+    # poz is positive or zero
+    poz = non_negative(x::AIFloat, stride)
+    
+    # signed layouts only use the first half as the positive side
+    nonneg = poz[1:end>>>1]
 
+    if Δ === :extended
+        # highest positive gets +Inf
+        nonneg[end] = T(Inf)
+    end
+
+    # negatives are the positives except the first (to avoid -0)
+    neg = -one(T) .* nonneg[2:end]
+
+    # signed layout: [+..., NaN, -...]
+    return vcat(nonneg, T(NaN), neg)
+end
+
+function non_negative(x::AIFloat, stride)
     exponent_values   = exponents(x, stride)
     fractional_values = fractionals(x)
     implicit_bits     = implicits(x)
 
     significand_values = fractional_values .+ implicit_bits
-    allpos = exponent_values .* significand_values
-
-    # signed layouts only use the first half as the positive side
-    # (your struct makes n_values the total encodings)
-    pos = allpos[1:end>>>1]
-
-    if Δ === :extended
-        # highest positive gets +Inf
-        pos[end] = T(Inf)
-    end
-
-    # negatives are the positives except the first (to avoid -0)
-    neg = -one(T) .* pos[2:end]
-
-    # signed layout: [+..., NaN, -...]
-    return vcat(pos, T(NaN), neg)
+    exponent_values .* significand_values
 end
+
