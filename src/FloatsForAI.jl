95e3ff6820ead499c6aca2d546bbaace9cb28ed8
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2026-02-25 13:41:10 -0500

    Update FloatsForAI.jl

--- src/FloatsForAI.jl
+++ src/FloatsForAI.jl
@@ -2,11 +2,15 @@ module FloatsForAI
 
 export AIFloat, codes, values
 
+include("types.jl")
+
+#=
 include("constructors.jl")
 include("abstract_and_concrete.jl")
 
 include("codes_and_values.jl")
 include("zero_and_larger_values.jl")
-
+=#
 
 end # module FloatsForAI
+
