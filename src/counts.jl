e998dab74880f1e38ded9b7be77c38ed7aa711ef
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2026-02-26 02:48:23 -0500

    Update counts.jl

--- src/counts.jl
+++ src/counts.jl
@@ -20,6 +20,8 @@ nPositiveSubnormalsOf(x::Format) = let P = PrecisionOf(x);
     twopowm1(P) - 1
 end
 
+nTotalSubnormalsOf(x::Format) = SignMultiplicityOf(x) * nPositiveSubnormalsOf(x)
+
 nNegativeSubnormalsOf(x::Format) = is_signed(x) * nPositiveSubnormalsOf(x)
 
 nSubnormalsOf(x::Format) = twopow(PrecisionOf(x) - is_unsigned(x)) - 1 - is_signed(x)
@@ -32,6 +34,36 @@ nNormalsOf(x::Format) = nFinitesOf(x) - nPrenormalsOf(x)
 nPositiveNormalsOf(x::Format) = nPositiveFinitesOf(x) - nNonnegativePrenormalsOf(x)
 nNegativeNormalsOf(x::Format) = is_signed(x) * nPositiveNormalsOf(x)
 
+#=
+Let 𝐸max_finite be the maximum biased exponent field value that is still usable by finite normals
+    (i.e., the highest exponent-field value that is not “lost” to NaN/Inf encodings in the low-precision corner cases)
+=#
+
+nPosSubnormalsOf(x::Format) = twopowm1(Precision(x)) - 0x01
+nSubnormalsOf(x::Format) = SignMultiplicityOf(x) * nPosSubnormalsOf(x)
+
+nPosNormalsOf(x::Format) = twopowm1(Precision(x)) * Emax_finite(x)
+nNormalsOf(x::Format) = SignMultiplicityOf(x) * nPosNormalsOf(x)
+
+function Emax_finite(x::Format)
+    P = Precision(x)
+    emaxfinite = twopow_expbits(x) - 0x01
+    if is_signed(x)
+        emaxfinite -= isone(P)
+    else
+        emaxfinite -= (P <= 2)
+    end
+    emaxfinite
+end
+
+twopow_expbits(x::Format) = twopow(ExponentFieldBitsOf(x))
+
+#=
+Emax_finite_uf(x::Format) = twopow_expbits(x) - 0x01 - isone(Precision(x))
+Emax_finite_ue(x::Format) = twopow_expbits(x) - 0x01 - (Precision(x) == 2) - (Precision(x) == 1)
+Emax_finite_sf(x::Format) = twopow_expbits(x) - 0x01
+Emax_finite_se(x::Format) = twopow_expbits(x) - 0x01 - (Precision(x) == 1)
+=#
 
 
 
